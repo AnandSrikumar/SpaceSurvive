cd630fc6cb0e7c15cf7754340824d0a6ef61daca
Anand <anand.srikumar007@gmail.com>
2020-02-16 16:16:10 +0530

    Optimized code

--- Practice.py
+++ Practice.py
@@ -55,7 +55,7 @@ boss = False
 music_loaded = True
 music = [Sprites.music, Sprites.boss_music]
 music_index = 0
-platformer = True
+platformer = False
 stage_x = -1500
 stage_y = -1000
 max_stage_x = 75000
@@ -364,7 +364,7 @@ def draw_enemy_platform():
 def collision_detection_bullet(en_rect, e_list):
     global bullet_list, score, explosions
     for bullet in bullet_list:
-        bul_rect = pygame.Rect(bullet[0]-bul_wid/6, bullet[1]-bul_hie/6, bul_wid/6, bul_hie/6)
+        bul_rect = pygame.Rect(bullet[0]-bul_wid, bullet[1]-bul_hie, bul_wid, bul_hie)
         if type(bul_rect) == pygame.Rect and bul_rect.colliderect(en_rect):
             e_list[6] -= 25
             bullet[0] = -2000
